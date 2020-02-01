8e6197ee38d258f22fe6914b81df6eb7b9bf7e53
William Lin <willlin1996@gmail.com>
2020-02-01 18:10:18 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,6 +1,6 @@
-def square_array(numbers)
-  numbers.each do |index|
-new_numbers.unshift numbers.pop**2
-  end
-end
+# def square_array(numbers)
+#   numbers.each do |index|
+# new_numbers.unshift numbers.pop**2
+#   end
+# end
 
